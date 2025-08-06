77e53607292103903e263f5f1eacc70e0a9822ee
bahico12 <ikromov0312@gmail.com>
2025-08-06 18:58:17 +0500

    added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#58B3D3', // Replace with your desired secondary color hex code
+      },
+    },
+  },
+  // ... other configurations
+};
